918dfba4151a0d586f0d7ae2efc06605a0c5304b
MarynaSobol <232347940+MarynaSobol@users.noreply.github.com>
2026-04-15 16:00:09 +0100

    update with a sub part 4
    update with a sub part4

--- practical_09_part4/add.asm
+++ practical_09_part4/add.asm
@@ -8,3 +8,4 @@ add:
     add eax, [esp + 8]   ; Add 2nd argument (20)
     add eax, [esp + 12]  ; Add 3rd argument (30)
     ret                  ; Result is returned in EAX
+section .note.GNU-stack noalloc noexec nowrite progbits
--- practical_09_part4/sub.asm
+++ practical_09_part4/sub.asm
@@ -7,3 +7,4 @@ sub:
     mov eax, [esp + 4]   ; Get 1st argument (50)
     sub eax, [esp + 8]   ; Subtract 2nd argument (20)
     ret                  ; Result is returned in EAX
+section .note.GNU-stack noalloc noexec nowrite progbits
